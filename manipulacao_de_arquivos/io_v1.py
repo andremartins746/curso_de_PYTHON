9c720053a60549b980d62f7412d5775274227982
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2020-11-27 11:19:48 -0200

    manipulação de arquivos csv

---
+++ manipulacao_de_arquivos/io_v1.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python2
+arquivo = open('pessoas.csv')
+dados = arquivo.read()
+arquivo.close()
+
+for registro in dados.splitlines():
+    print("nome: {}, idade: {}".format(*registro.split(',')))
---
+++ manipulacao_de_arquivos/io_v2.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python2
+
+arquivo = open('pessoas.csv')
+
+for registro in arquivo:
+    print('nome: {}, idade: {}'.format(*registro.split(',')))
+
+arquivo.close()
